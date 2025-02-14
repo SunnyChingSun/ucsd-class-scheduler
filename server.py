eeefb8570b9eb8dd3cdb28a8c596f0fa4b4965c6
Sunny Sun <150289426+SunnyChingSun@users.noreply.github.com>
2025-02-13 22:57:41 -0800

    Update server.py

--- server.py
+++ server.py
@@ -1,1 +1,11 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return jsonify({"message": "Hello, UCSD Enrollment System!"})
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
